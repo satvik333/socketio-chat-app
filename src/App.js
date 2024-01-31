48696e05ce61c323ad1ab0d4f1b73abcadd463d9
aalesh01 <aalesh111999@gmail.com>
2024-01-31 16:52:22 +0530

    added dev dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import LoginPage from './components/loginPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ChatPage from './components/chatPage';
-import React, { useState } from 'react';
-import socketIO from 'socket.io-client';
+import LoginPage from "./components/loginPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ChatPage from "./components/chatPage";
+import React, { useState } from "react";
+import socketIO from "socket.io-client";
 
 function App() {
-  const socket = socketIO.connect('http://172.16.0.132:3001');
+  const socket = socketIO.connect("http://172.16.0.132:3001");
   const [loggedInUser, setLoggedInUser] = useState(null);
 
   const handleLogin = (user) => {
@@ -15,8 +15,16 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/login" element={<LoginPage onLogin={handleLogin} />} />
-        <Route exact path="/chat" element={<ChatPage loggedInUser={loggedInUser} socket={socket} />} />
+        <Route
+          exact
+          path="/login"
+          element={<LoginPage onLogin={handleLogin} />}
+        />
+        <Route
+          exact
+          path="/chat"
+          element={<ChatPage loggedInUser={loggedInUser} socket={socket} />}
+        />
       </Routes>
     </Router>
   );
